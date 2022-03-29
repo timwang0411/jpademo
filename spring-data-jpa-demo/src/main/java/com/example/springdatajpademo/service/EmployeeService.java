58490b648d6b01b44f804a5d8d0f079697dbec94
FAREASTONE\timwang <timwang@fareastone.com.tw>
2022-03-29 17:25:32 +0800

    EmployeeService update

--- spring-data-jpa-demo/src/main/java/com/example/springdatajpademo/service/EmployeeService.java
+++ spring-data-jpa-demo/src/main/java/com/example/springdatajpademo/service/EmployeeService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.util.List;
 
